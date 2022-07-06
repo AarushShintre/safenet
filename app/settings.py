625fa4c1944512aa2e0b7d1d7bb6e606024751e4
Aarush Shintre <65198209+AarushShintre@users.noreply.github.com>
2022-07-06 23:27:43 +0530

    heroku

--- app/settings.py
+++ app/settings.py
@@ -1,4 +1,9 @@
+from enum import Flag
 import os
 
 SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 SECRET_KEY = os.environ.get("SECRET_KEY")
+
+Debug = False
+
+Allowed_hosts = ['https://safenet-app.herokuapp.com/']
